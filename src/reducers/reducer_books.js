82d34c6cbcd99d13c902835ea023dfa0c5ced549
Karlo B <karlo@neuralab.net>
2017-09-22 10:40:52 +0200

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: 'Javascript: The Good Parts', pages: 788},
+		{ title: 'Harry Potter', pages: 874},
+		{ title: 'The Dark Tower', pages: 48},
+		{ title: 'Eloquent Ruby', pages: 1}
+	]
+}
